837e9fb0451e870595dd44a372795d6cb03bb1f4
stevenpfaff <pfaffy1426@gmail.com>
2022-04-26 12:43:37 -0500

    test commit with player controller and get request working

---
+++ APIJhawks/Controllers/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace APIJhawks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public PlayerController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                select PlayerId, PlayerName from dbo.Player";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("PlayerAppCon");
+            SqlDataReader myReader;
+            using(SqlConnection myCon=new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ APIJhawks/Models/Players.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIJhawks.Models
+{
+    public class Players
+    {
+        public int PlayerId { get; set; }
+
+        public string PlayerName { get; set; }
+    }
+}
