0de4623e9976e8ce6469d75c77fdf0049cca56d5
stevenpfaff <pfaffy1426@gmail.com>
2022-07-04 15:35:17 -0500

    commiting recent adds

---
+++ APIJhawks/Controllers/TeamTotalsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using APIJhawks.Models;
+
+namespace APIJhawks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamTotalsController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+        public TeamTotalsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                select * from dbo.Team";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("PlayerAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+            return new JsonResult(table);
+        }
+    }
+}
+
---
+++ APIJhawks/Models/TeamTotals.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIJhawks.Models
+{
+    public class TeamTotals
+    {
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public int Ties { get; set; }
+        public int R { get; set; }
+        public int RA { get; set; }
+        public int RD { get; set; }
+        public int H { get; set; }
+        public float BA { get; set; }
+        public float OBP { get; set; }
+        public float SLG { get; set; }
+        public float OPS { get; set; }
+        public float INN { get; set; }
+        public float ERA { get; set; }
+        public float WHIP { get; set; }
+        public int BB { get; set; }
+        public int K { get; set; }
+        public float FIP { get; set; }
+        public int FP { get; set; }
+
+    }
+}
